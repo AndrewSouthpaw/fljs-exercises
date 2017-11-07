695e36bba1b8a4726caf7cbfd5c51776f94f0a31
Andrew Smith <andrewsmith@alumni.stanford.edu>
2017-11-07 13:14:51 -0800

    Finish exercise 3.

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,16 +1,36 @@
-function increment(x) { return x + 1; }
-function decrement(x) { return x - 1; }
-function double(x) { return x * 2; }
-function half(x) { return x / 2; }
+function increment(x) { return x + 1 }
 
-function compose() {}
-function pipe() {}
+function decrement(x) { return x - 1 }
 
-var f = compose(decrement,double,increment,half);
-var p = pipe(half,increment,double,decrement);
+function double(x) { return x * 2 }
 
-f(3) === 4;
+function half(x) { return x / 2 }
+
+const compose = (...fns) => (
+  (...args) => {
+    const recurse = (...rest) => {
+      if (rest.length === 1) return rest[0](...args)
+      return rest[0](recurse(...rest.slice(1)))
+    }
+
+    return recurse(...fns)
+  }
+)
+
+const pipe = (...fns) => (
+  (...args) => {
+    if (fns.length === 1) return fns[0](...args)
+    return pipe(...fns.slice(1))(fns[0](...args))
+  }
+)
+
+var f = compose(decrement, double, increment, half)
+var p = pipe(half, increment, double, decrement)
+
+f(3) === 4
 // true
+console.log(f(3) === 4)
 
-f(3) === p(3);
+f(3) === p(3)
+console.log(f(3) === p(3))
 // true
