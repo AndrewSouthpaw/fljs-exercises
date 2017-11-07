b4fbfe74e1fdd2ec2f3397aaa69d6e6a09536c13
Andrew Smith <andrewsmith@alumni.stanford.edu>
2017-11-07 13:39:58 -0800

    Finish exercise 4.

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,13 +1,16 @@
 function lotteryNum() {
-	return (Math.round(Math.random() * 100) % 58) + 1;
+  return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber(){}
+function pickNumber(numbers) {
+  return numbers.concat(lotteryNum()).sort((a, b) => a > b)
+}
 
 var luckyLotteryNumbers = [];
 
 for (var i = 0; i < 6; i++) {
-	pickNumber();
+  luckyLotteryNumbers = pickNumber(luckyLotteryNumbers);
 }
 
 console.log(luckyLotteryNumbers);
+
