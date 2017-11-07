a0c4d6febcf4839faa28da9a64330ad1a2b1616b
Andrew Smith <andrewsmith@alumni.stanford.edu>
2017-11-07 11:39:22 -0800

    Finish exercise 1.

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -3,6 +3,15 @@ function foo(x) {
 	z = x * y;
 }
 
+function Foo(curX, curY) {
+  var [origY, origZ] = [y, z]
+  y = curY
+  foo(curX, y)
+  var [newY, newZ] = [y, z]
+  [y, z] = [origY, origZ]
+  return [newY, newZ]
+}
+
 var y = 5, z;
 
 foo(20);
